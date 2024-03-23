b39e895726f8003ed49b748cce5d4b11b4966455
sanchosremy <sachenkaremy@gmail.com>
2024-03-23 12:01:54 +0100

    project_task_predicitons

---
+++ task2.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+music_data = pd.read_csv('music.csv')
+X = music_data.drop(columns=['genre'])
+print(X)
+
+Y = music_data['genre']
+print(Y)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+model = DecisionTreeClassifier()
+model.fit(X_train, Y_train)
+predictions = model.predict(X_test)
+print(predictions)
+
+score = accuracy_score(Y_test, predictions)
+print(score)
